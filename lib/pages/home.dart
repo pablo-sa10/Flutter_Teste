a3e9aac34d96b8b58969d615d4918c6c62e12bb3
Pablo <pablomoura2016@gmail.com>
2024-09-07 23:24:26 -0300

    criei a linha onde o valor será exibido

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -19,21 +22,49 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
+        padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
         child: ListView(
           children: [
             Container(
               height: 500,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-                color: Colors.blueGrey
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [Text("Saldo disponivel"),],
-                  ),
-                ],
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.blueGrey),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Saldo disponível",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "R\$",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8),
+                                  child: Text("Valor"),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        Icon(Icons.arrow_forward_ios_rounded)
+                      ],
+                    ),
+                  ],
+                ),
               ),
             )
           ],
